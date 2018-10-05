0fec9b4675d012cb018b3083066fddf33c324943
wayleung <610686949@qq.com>
2018-10-05 10:44:44 +0800

    add id getter and setter ,fix the bug

--- springboot-demo/src/main/java/com/way/springbootdemo/dto/User.java
+++ springboot-demo/src/main/java/com/way/springbootdemo/dto/User.java
@@ -43,32 +43,40 @@ public class User implements Serializable {
         this.birthdate = birthdate;
     }
 
-    public String getUsername() {
-        return username;
+    public Integer getId() {
+        return id;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public String getUsername() {
+        return username;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public Integer getGender() {
         return gender;
     }
 
-    public void setGender(Integer gender) {
-        this.gender = gender;
-    }
-
     public Long getBirthdate() {
         return birthdate;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
     }
 
     public void setBirthdate(Long birthdate) {
@@ -78,12 +86,11 @@ public class User implements Serializable {
     @Override
     public String toString() {
         return "User{" +
-                "username='" + username + '\'' +
+                "id=" + id +
+                ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 ", gender=" + gender +
                 ", birthdate=" + birthdate +
                 '}';
     }
-
-
 }
