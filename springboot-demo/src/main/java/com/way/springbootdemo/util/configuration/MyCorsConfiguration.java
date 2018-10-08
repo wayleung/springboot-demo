7cf0d625baacf796733ef155cf39ca44400c79b3
wayleung <610686949@qq.com>
2018-10-08 17:06:54 +0800

    close Springboot devTools and test CORS successfully

--- springboot-demo/src/main/java/com/way/springbootdemo/util/configuration/MyCorsConfiguration.java
+++ springboot-demo/src/main/java/com/way/springbootdemo/util/configuration/MyCorsConfiguration.java
@@ -11,26 +11,33 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @Description:
  */
 @Configuration
-public class MyCorsConfiguration{
+public class MyCorsConfiguration {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer(){
+    public WebMvcConfigurer getWebMvcConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                //registry.addMapping("/api/**").allowedOrigins("http://localhost:8080");
-                //添加映射路径
-                registry.addMapping("/**")
-                        //放行哪些原始域
-                        .allowedOrigins("*")
-                        //是否发送Cookie信息
-                        .allowCredentials(true)
-                        //放行哪些原始域(请求方式)
-                        .allowedMethods("GET","POST", "PUT", "DELETE")
-                        //放行哪些原始域(头部信息)
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("PUT", "DELETE", "GET", "POST")
                         .allowedHeaders("*")
-                        //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
-                        .exposedHeaders("Header1", "Header2");
+                        .exposedHeaders("access-control-allow-headers",
+                                "access-control-allow-methods",
+                                "access-control-allow-origin",
+                                "access-control-max-age",
+                                "X-Frame-Options")
+                        .allowCredentials(false).maxAge(3600);
+//                //添加映射路径
+//                registry.addMapping("/**")
+//                        //放行哪些原始域
+//                        .allowedOrigins("*")
+//                        //是否发送Cookie信息
+//                        .allowCredentials(true)
+//                        //放行哪些原始域(请求方式)
+//                        .allowedMethods("GET","POST", "PUT", "DELETE")
+//                        //放行哪些原始域(头部信息)
+//                        .allowedHeaders("*")
+//                        //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+//                        .exposedHeaders("Header1", "Header2");
             }
         };
     }
